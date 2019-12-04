7250a2369f6e7c41767e55e45a5fee1276d33375
anstnsp <anstnsp@naver.com>
2019-12-05 01:50:53 +0900

    fetch data of movies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
+class App extends React.Component {
 
+  state = { 
+    isLoading : true,
+    movies : [], 
+  }
 
-function App() {
-  return (
-    <div className="App">
-     hello
-    </div>
-  );
+  getMovies = async () => {
+    const { data : {
+      data : { movies } 
+    }} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({
+      movies, 
+      isLoading : false,
+    })
+  };
+  componentDidMount() {
+      this.getMovies();
+  }
+  render () {
+    const { isLoading , movies } = this.state;
+    return(
+      <div>{isLoading ? "Loding..." : movies.map(movie => (
+         <Movie key={movie.id}
+                id={movie.id} 
+                year={movie.year} 
+                title={movie.title} 
+                summary={movie.summary} 
+                poster={movie.medium_cover_image}/>
+      ))}</div>
+    )
+  }
 }
 
 export default App;
+
+//https://yts-proxy.now.sh/list_movies.json
+//https://github.com/serranoarevalo/yts-proxy
